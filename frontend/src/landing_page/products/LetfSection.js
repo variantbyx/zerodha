b7de046b51cec18814ba4de82b70769f5435d302
techielukesh <alltechfactzzz@gmail.com>
2025-12-27 16:21:12 +0530

    product page setup

--- frontend/src/landing_page/products/LetfSection.js
+++ frontend/src/landing_page/products/LetfSection.js
@@ -1,10 +1,44 @@
 import React from "react";
 
-export default function LetfSection() {
+function LeftSection({
+  imageURL,
+  productName,
+  productDesription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
   return (
-    <div className="left-section">
-      <h4>Left Section</h4>
-      <p>Placeholder left content for products.</p>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6">
+          <img src={imageURL} />
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDesription}</p>
+          <div>
+            <a href={tryDemo}>Try Demo</a>
+            <a href={learnMore} style={{ marginLeft: "50px" }}>
+              Learn More
+            </a>
+          </div>
+          <div className="mt-3">
+            <a href={googlePlay}>
+              <img src="media/images/googlePlayBadge.svg" />
+            </a>
+            <a href={appStore}>
+              <img
+                src="media/images/appstoreBadge.svg"
+                style={{ marginLeft: "50px" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default LeftSection;
--- frontend/src/landing_page/products/RightSection.js
+++ frontend/src/landing_page/products/RightSection.js
@@ -1,10 +1,22 @@
 import React from "react";
 
-export default function RightSection() {
+function RightSection({ imageURL, productName, productDesription, learnMore }) {
   return (
-    <div className="right-section">
-      <h4>Right Section</h4>
-      <p>Placeholder right content for products.</p>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDesription}</p>
+          <div>
+            <a href={learnMore}>Learn More</a>
+          </div>
+        </div>
+        <div className="col-6">
+          <img src={imageURL} />
+        </div>
+      </div>
     </div>
   );
 }
+
+export default RightSection;
