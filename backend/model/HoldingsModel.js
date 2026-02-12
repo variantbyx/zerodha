4d82ec9fc9dd9a0af5effb8ad48d1bc68a22e7f2
techielukesh <alltechfactzzz@gmail.com>
2026-02-12 21:25:18 +0530

    first commit

--- backend/model/HoldingsModel.js
+++
@@ -1,7 +0,0 @@
-const { model } = require("mongoose");
-
-const { HoldingsSchema } = require("../schemas/HoldingsSchema");
-
-const HoldingsModel = model("holding", HoldingsSchema);
-
-module.exports = { HoldingsModel };
